d8ac21e03c6824d3dec7839a02fbdf8fc9f93042
JAYESH <jayesh.p.patel24@gmail.com>
2024-09-04 11:18:23 +0530

    Test

---
+++ demo.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int sum(int x,int y)
+{
+	int z=x+y;
+	return z;
+}
+int main()
+{
+	int a,b;
+	printf("Enter : ");
+	scanf("%d %d",&a,&b);
+	int c=sum(a,b);
+	printf("sum = %d",c);
+    return 0;
+}
+
